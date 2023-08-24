21bf4ee91d35cfeed98d261de349311d5c4b732e
SudeepTrainer <info@icenbake.com>
2023-08-24 17:53:10 +0530

    24-08-2023

--- Javascript/Object.js
+++ Javascript/Object.js
@@ -16,5 +16,91 @@ console.log(elonObject.skills[1]);
 // create an object for shoppingcart
 //[{},{},{}]
 
+// objects are references 
+const product1 = {
+    name:"Keyboard",
+    price:2456
+}
 
+const product2 = {
+    name:"Keyboard",
+    price:2456
+}
 
+console.log(product1 === product2);
+
+// product3 = product2;
+
+// product3.name = "Laptop";
+// console.log(product2);
+
+// let product3 = Object.assign({},product2); //clone an object
+// product3.name = "Laptop";
+// console.log(product2);
+
+let product3 = {...product2}; // clone an object with spread operator
+product3.name = "Laptop";
+console.log(product3);
+console.log(product2);
+
+let product = {} 
+
+product.name = "TV";
+product.price = 234234;
+console.log(product);
+
+let person ={
+    firstName:"Mark",
+    lastName:"Zuck",
+    // getFullName:function getFullName(){
+    //     return `${this.firstName} ${this.lastName}`
+    // }
+    // getFullName:function (){
+    //     return `${this.firstName} ${this.lastName}`
+    // }
+    getFullName(){
+       return `${this.firstName} ${this.lastName}`
+    },
+    greet(){
+        return `Hi, this is ${this.firstName}`
+    }
+}
+
+console.log(person.getFullName());
+console.log(person.greet());
+
+// function Person(firstName,lastName){
+//     let person = {};
+//     person.firstName = firstName;
+//     person.lastName = lastName;
+//     person.getFullName = function(){
+//         return `${this.firstName} ${this.lastName}`
+//      };
+//     person.greet = function(){
+//         return `Hi, this is ${this.firstName}`
+//     }
+//     return person;
+// }
+
+// let elon = Person("Elon","Musk");
+// console.log(elon.greet());
+
+const personMethods = {
+    getFullName:function(){
+        return `${this.firstName} ${this.lastName}`
+     },
+    greet :function(){
+        return `Hi, this is ${this.firstName}`
+    }
+}
+function Person(firstName,lastName){
+    let person = {};
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.getFullName = personMethods.getFullName;
+    person.greet = personMethods.greet;
+    return person;
+}
+
+let elon = Person("Elon","Musk");
+console.log(elon.greet());
--- ReactExamples/FirstApp/src/main.jsx
+++ ReactExamples/FirstApp/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function FirstPage(){
+function Header(){
   return(
-    <h1>Hello World!</h1>
+    <h2>Header component</h2>
   )
 }
-
+function Content(){
+  return(
+    <body>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, ad iusto! Saepe officia exercitationem dolore iste, quisquam quia velit, hic distinctio quos, maiores a. Nam quos ducimus id distinctio alias.
+    </body>
+  )
+}
+function Footer(){
+  return(
+    <small>Footer</small>
+  )
+}
+function FirstPage(){
+  return(
+    <div>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </div>
+  )
+}
 ReactDOM.createRoot(document.getElementById('root')).render(<FirstPage />)
