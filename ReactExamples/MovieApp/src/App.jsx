10e93696d7a420d13e149ae1dbac5bc8f9356cd2
SudeepTrainer <info@icenbake.com>
2023-10-09 16:45:03 +0530

    9-10-23

--- ReactExamples/MovieApp/src/App.jsx
+++ ReactExamples/MovieApp/src/App.jsx
@@ -1,7 +1,10 @@
+import SearchMovie from "./components/SearchMovies"
+
 function App() {
   return (
     <div>
       <h1>Movie Search App</h1>
+      <SearchMovie/>
     </div>
   )
 }
--- ReactExamples/MovieApp/src/components/SearchMovies.jsx
+++ ReactExamples/MovieApp/src/components/SearchMovies.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
+
 export default function SearchMovie(){
+
+    const [movieName,setMovieName] = useState("");
+    const [movies,setMovies] = useState([]);
+
+    function fetchMovies(event){
+        event.preventDefault();
+        const url = `https://api.themoviedb.org/3/search/movie?&api_key=24d1042ae5621a567de03462da33bd83&query=${movieName}&include_adult=false&language=en-US&page=1`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setMovies(data.results));
+    }
+
+    function handleInput(event){
+        // console.log(event.target.value);
+        setMovieName(event.target.value);
+    }
 
     return(
         <>
-        <form className="form" >
+        <form className="form" onSubmit={fetchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query" 
-                    placeholder="Enter movie name" />
+                    placeholder="Enter movie name" value={movieName} onChange={(handleInput)}/>
             <button className="button" type="submit">Search</button>
         </form>
+        <div>
+            {movies.map(movie => <h3>{movie.title}</h3>)}
+        </div>
         </>
     )
 }
